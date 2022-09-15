039685519ac5399039fe48af5dfb59c0cfd343e9
AugustoNoronha <64671692+AugustoNoronha@users.noreply.github.com>
2022-09-15 14:37:22 -0300

    request

--- site/services/api.js
+++ site/services/api.js
@@ -1,34 +1,38 @@
+const card = document.getElementById("card");
+
+window.onload = () => {
+  main();
+};
+
 function get(url) {
   let request = new XMLHttpRequest();
   request.open("GET", url, false);
   request.send();
   return request.responseText;
-}   
-
-function makeLine(prod) {
-    linha = document.createElement('tr')
-    tdId =  document.createElement('td')
-    tdNome =  document.createElement('td')
-    tdId.innerHTML = prod.id
-    tdNome.innerHTML = prod.modelo
-
-    linha.appendChild(tdId);
-    linha.appendChild(tdNome);
-
-    return linha;
-
 }
+
 
 function main() {
-  data = get("http://localhost:3001/roupas");
+  data = get("http://localhost:3001/livros");
   telefones = JSON.parse(data);
-  console.log(telefones)
-  let table = document.getElementById('table')
+  console.log(telefones);
+  card.innerHTML = "";
 
   telefones.forEach((element) => {
-    let line = makeLine(element);
-    table.appendChild(line)
+    let modelo = element.modelo;
+    let preco = element.preco;
+    let img = element.img;
+    let link = element.link;
+    let desc = element.desc;
+    container = `
+    <div id='sigle'>
+          <p class="item-valor">${modelo}</p>
+          <p class="item-valor">${preco}</p>
+          <img id="img-size" src="${img}">
+          <a href="${link}"> link da loja </a>
+
+    </div>
+    `;
+    card.innerHTML += container
   });
 }
-
-main();
