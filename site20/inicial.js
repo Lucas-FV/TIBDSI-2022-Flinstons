0336bdc24b0806ce98813e5402688ced34a54b18
AugustoNoronha <64671692+AugustoNoronha@users.noreply.github.com>
2022-09-15 15:52:43 -0300

    initial page

--- site20/inicial.js
+++ site20/inicial.js
@@ -1,4 +1,42 @@
-localStorage.setItem('locate', null)
-function getButton(locate){
-    localStorage.setItem("locate", locate)
+window.onload = () => {
+
+  main();
+};
+
+function get(url) {
+  let request = new XMLHttpRequest();
+  request.open("GET", url, false);
+  request.send();
+  return request.responseText;
+}
+
+function main() {
+  const card = document.getElementById("row");
+
+  data = get("http://localhost:3001/perfume");
+  telefones = JSON.parse(data);
+  console.log(telefones);
+  card.innerHTML = "";
+
+  telefones.forEach((element) => {
+    let modelo = element.modelo;
+    let preco = element.preco;
+    let img = element.img;
+    let link = element.link;
+    let desc = element.desc;
+
+    var f = preco.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+    container = `
+
+    <div class="cardI green" id="card">
+        <img class="image" src="${img}" alt="money" />
+        <h2>${modelo}</h2>
+        <p>${f}</p>
+        <a class="loja" href="${link}"> Acesso a loja </a>
+
+    </div>
+
+    `;
+    card.innerHTML += container;
+  });
 }
